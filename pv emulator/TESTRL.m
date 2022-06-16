1f592ed9c2981fbf3096021fa25d2da09ac6e73d
Joe <jahabchane3@gatech.edu>
2022-06-16 14:31:00 +0200

    test

---
+++ pv emulator/TESTRL.m
@@ -0,0 +1,4 @@
+R2=input('enter the value R2');
+R3=input('enter the value R3');
+R1=20;
+RL=((R1+R2)*R3)/(R1+R2+R3)
---
+++ pv emulator/testR3.m
@@ -0,0 +1,4 @@
+RL=input('enter the value RL');
+R2=input('enter the value R2');
+R1=20;
+R3=(RL*(R1+R2))/(R1+R2-RL)
