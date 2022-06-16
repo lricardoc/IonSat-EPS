b908bd903415e09e08b106ae0edfab88944625b1
Ricardo <ricardo.colpari@gmail.com>
2022-06-16 15:16:03 +0200

    added rl

--- pv emulator/TESTRL.m
+++ pv emulator/TESTRL.m
@@ -1,4 +1,13 @@
-R2=input('enter the value R2');
-R3=input('enter the value R3');
-R1=20;
-RL=((R1+R2)*R3)/(R1+R2+R3)
+function [RL,R3,I1,P1,P2,P3] = TESTRL(R1,R2, I, V)
+
+RL=V/I;
+R3=(RL*(R1+R2))/(R1+R2-RL);
+%V=I*RL;
+I3=V/R3;
+I1=I-I3;
+%I3=I-I1;
+P1=I1^2*R1;
+P2=I1^2*R2;
+P3=I3^2*R3;
+end
+
