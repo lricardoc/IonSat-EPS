bb17feb7b2edd82b4025d1da391b2a2786159f23
Ricardo <ricardo.colpari@gmail.com>
2022-06-24 10:27:32 +0200

    added live script files

--- pv emulator/TESTRL.m
+++ pv emulator/TESTRL.m
@@ -1,12 +1,10 @@
-function [RL,R3,I1,P1,P2,P3] = TESTRL(R1,R2, I, V)
+function [RL,I1,P1,P2,P3] = testRL(R1,R2,R3,I,V)
 
-RL=V/I;
-R3=(RL*(R1+R2))/(R1+R2-RL);
-%V=I*RL;
+RL=(R3.*(R1+R2))./(R1+R2+R3);
 I3=V/R3;
 I1=I-I3;
 %I3=I-I1;
-P1=I1^2*R1;
+P1=I1^2.*R1;
 P2=I1^2*R2;
 P3=I3^2*R3;
 end
